5270c6f8820e7bf5b051497066ce2c5d3945a7b6
Justyna Krzywda <justyna.krzywda@gmail.com>
2020-05-04 11:19:54 +0100

    cleaning

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 
-import reducers from "./reducers/index";
-import App from "./App";
+import reducers from "./reducers";
+import App from "./components/App";
 
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
