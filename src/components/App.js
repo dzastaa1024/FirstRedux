05dc7a9961ac39e4a0a56b06ec9562e0bbfe0522
Justyna Krzywda <justyna.krzywda@gmail.com>
2020-05-04 12:39:32 +0100

    calling action

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,12 @@ import SongList from "./SongList";
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <SongList />
+      <div className="ui content grid">
+        <div className="ui row">
+          <div className="column eight wide">
+            <SongList />
+          </div>
+        </div>
       </div>
     );
   }
