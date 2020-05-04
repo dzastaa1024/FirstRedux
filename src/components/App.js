eba0a39c00db7939829e5b3eb2adf711fb8ff744
Justyna Krzywda <justyna.krzywda@gmail.com>
2020-05-04 11:20:55 +0100

    SongList component

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,12 @@
 import React, { Component } from "react";
-import actions from "./actions/index";
+import SongList from "./SongList";
 
 export default class App extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <SongList />
+      </div>
+    );
   }
 }
